805a533210624244113236957355774992e1e3b2
Dolmayan <alejandrodolmayan@gmail.com>
2023-08-19 11:25:37 -0600

    Consumer Interface added

---
+++ src/main/java/com/java/dolmayan/JavaStreamsApi/ApiFundamentals/FunctionalInterfaces/BiConsumerExample.java
@@ -0,0 +1,40 @@
+package com.java.dolmayan.JavaStreamsApi.ApiFundamentals.FunctionalInterfaces;
+
+import com.java.dolmayan.JavaStreamsApi.ApiFundamentals.model.Student;
+import com.java.dolmayan.JavaStreamsApi.ApiFundamentals.model.StudentDataBase;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+
+public class BiConsumerExample {
+    /** Consumer Interface will only accept the input
+     * and it doesn't return any output out of that
+     */
+
+    public static void NameAndActivities(){
+
+        BiConsumer<String, List<String>> biConsumer = (name,activities) -> System.out.println(name + " : "+ activities);
+        List<Student> studentList = StudentDataBase.getAllStudents();
+
+        studentList.forEach(s -> biConsumer.accept(s.getName(), s.getActivities()));
+    }
+    public static void main(String[] args) {
+
+        BiConsumer<String, String> biConsumer = (a,b) ->{
+            System.out.println("a :" + a +", b :"+b );
+        };
+        biConsumer.accept("java7", "java8");
+
+        BiConsumer<Integer,Integer> multiply = (a,b) ->{
+            System.out.println("Multiplication is : " + (a*b));
+        };
+
+        BiConsumer<Integer,Integer> division = (a,b) ->{
+            System.out.println("Division is : " + (a/b));
+        };
+
+        //multiply.andThen(division).accept(10,5);
+
+        NameAndActivities();
+    }
+}
