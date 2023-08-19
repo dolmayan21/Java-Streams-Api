430f6dd8bef1521db700aaf45fc7b7378422d9ec
Dolmayan <alejandrodolmayan@gmail.com>
2023-08-18 22:00:22 -0600

    New Classes added

---
+++ src/main/java/com/java/dolmayan/JavaStreamsApi/ApiFundamentals/FunctionalInterfaces/ConsumerExample.java
@@ -0,0 +1,14 @@
+package com.java.dolmayan.JavaStreamsApi.ApiFundamentals.FunctionalInterfaces;
+
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+
+    public static void main(String[] args) {
+        Consumer<String> c1 = (s) -> System.out.println(s.toUpperCase());
+
+        c1.accept("java8");
+
+
+    }
+}
